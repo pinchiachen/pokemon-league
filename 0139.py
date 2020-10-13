d2eb24d892bd8419473e5f2267cf6fd91e538c19
pinchiachen <pcc@adenovo.com>
2020-10-13 17:40:51 +0800

    [2020-10-13] add 139

---
+++ 0139.py
@@ -0,0 +1,17 @@
+## 2020/10/13
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        l = len(s)
+        dp = [False] * (l+1)
+        dp[0] = True
+        wordDict = set(wordDict)
+        for i in range(1, l+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+        return dp[l]
+
+if __name__ == "__main__":
+    print(Solution().wordBreak("leetcode", ["leet","code"]))
