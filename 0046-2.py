14f7a3bce542b0bed11cbd912e58a0dcbbb68656
pinchiachen <pcc@adenovo.com>
2020-11-05 17:18:29 +0800

    [2020-11-05] add 46-2

---
+++ 0046-2.py
@@ -0,0 +1,15 @@
+## 2020/11/05
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums: return []
+        res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+            return
+        for i in range(len(nums)):
+            self.dfs(nums[:i] + nums[i+1:], path + [nums[i]], res)
