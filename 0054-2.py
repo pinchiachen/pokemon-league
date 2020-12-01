53312162f40cc8b028d917b222188db5d55e30d6
pinchiachen <pcc@adenovo.com>
2020-12-01 11:56:38 +0800

    [2020-12-01]  update 54-2

--- 0054-2.py
+++ 0054-2.py
@@ -7,16 +7,13 @@ class Solution:
         N = len(matrix[0])
         res = []
         visited = [[0] * N for _ in range(M)]
-        x = -1
-        y = -1
+        directions = ['right', 'bottom', 'left', 'top']
+        x = 0
+        y = 0
         while True:
-            is_end, x, y = self.goStraight(matrix, x+1, y+1, M, N, 'right', visited, res)
-            if is_end: break
-            is_end, x, y = self.goStraight(matrix, x+1, y-1, M, N, 'bottom', visited, res)
-            if is_end: break
-            is_end, x, y = self.goStraight(matrix, x-1, y-1, M, N, 'left', visited, res)
-            if is_end: break
-            is_end, x, y = self.goStraight(matrix, x-1, y+1, M, N, 'top', visited, res)
+            for direction in directions:
+                is_end, x, y = self.goStraight(matrix, x, y, M, N, direction, visited, res)
+                if is_end: break
             if is_end: break
         return res
 
@@ -24,22 +21,28 @@ class Solution:
         is_end = True
         while 0 <= x < M and 0 <= y < N and not visited[x][y]:
             is_end = False
+            res.append(matrix[x][y])
+            visited[x][y] = 1
             if direction == 'right':
-                res.append(matrix[x][y])
-                visited[x][y] = 1
                 y += 1
             elif direction == 'left':
-                res.append(matrix[x][y])
-                visited[x][y] = 1
                 y -= 1
             elif direction == 'bottom':
-                res.append(matrix[x][y])
-                visited[x][y] = 1
                 x += 1
             elif direction == 'top':
-                res.append(matrix[x][y])
-                visited[x][y] = 1
                 x -= 1
+        if direction == 'right':
+            x += 1
+            y -= 1
+        elif direction == 'left':
+            x -= 1
+            y += 1
+        elif direction == 'bottom':
+            x -= 1
+            y -= 1
+        elif direction == 'top':
+            x += 1
+            y += 1
         return (is_end, x, y)
 
 if __name__ == "__main__":
