52e1c577d5d6651cdac417e08bfc7c25ad71ed02
pinchiachen <pcc@adenovo.com>
2020-12-02 17:41:18 +0800

    [2020-12-02] add 56-2

---
+++ 0056-2.py
@@ -0,0 +1,12 @@
+## 2020/12/02
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x: x[0])
+        res = [intervals[0]]
+        for interval in intervals[1:]:
+            if interval[0] > res[-1][1]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+        return res
