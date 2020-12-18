6a44d883ab6045a81faa39dc8a2616a6959a3740
pinchiachen <pcc@adenovo.com>
2020-12-18 14:24:04 +0800

    [2020-12-18] add 206-2

---
+++ 0206-2.py
@@ -0,0 +1,16 @@
+## 2020/12/18
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = None
+        while head:
+            cur = head
+            head = head.next
+            cur.next = pre
+            pre = cur
+        return pre
