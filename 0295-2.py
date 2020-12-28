3fd3a740939c708869b756497e00cfed152e2075
pinchiachen <pcc@adenovo.com>
2020-12-28 17:25:42 +0800

    [2020-12-28] add 295-2

---
+++ 0295-2.py
@@ -0,0 +1,22 @@
+## 2020/12/28
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.max_heap = []
+        self.min_heap = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.min_heap, -heapq.heappushpop(self.max_heap, -num))
+        if len(self.min_heap) > len(self.max_heap):
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+
+    def findMedian(self) -> float:
+        if len(self.min_heap) == len(self.max_heap):
+            return (self.min_heap[0] - self.max_heap[0]) / 2
+        else:
+            return -self.max_heap[0]
