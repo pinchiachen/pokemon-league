4f11b9e3e83b5fa815f9a2d6c429348a7f9fcd20
pinchiachen <pcc@adenovo.com>
2020-12-28 16:22:35 +0800

    [2020-12-28] add 268

---
+++ 0268.py
@@ -0,0 +1,8 @@
+## 2020/12/28
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+        for i in range(len(nums)):
+            res += (i - nums[i])
+        return res
