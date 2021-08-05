4d6e4b6b95f9b35dbc99e5f487113b90d13b74aa
pinchiachen <chenargar@gmail.com>
2021-08-05 16:33:09 +0800

    [2021-08-05] add 112-3

---
+++ 0112-3.py
@@ -0,0 +1,8 @@
+## 2021/08/05
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root: return False
+        if root.val == sum and not root.left and not root.right:
+            return True
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
