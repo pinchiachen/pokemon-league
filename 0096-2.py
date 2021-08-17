01243108fd6c2c7b3080cb3a803c68aae8dc9b28
pinchiachen <pcc@aifian.com>
2021-08-17 16:14:46 +0800

    [2021-08-17] update 96-2

--- 0096-2.py
+++ 0096-2.py
@@ -1,22 +1,19 @@
-## 2021/08/16
+## 2021/08/17
 
 class Solution:
     def numTrees(self, n: int) -> int:
-        arr = [i for i in range(1, n+1)]
-        return self.calculatePath(arr, {})
+        return self.calculatePath(n, {})
 
-    def calculatePath(self, arr, exist_map):
-        if not arr or len(arr) == 1:
-            return 1
+    def calculatePath(self, n, exist_map):
+        if n <= 1: return 1
+        if n in exist_map:
+            return exist_map[n]
         res = 0
-        map_str = f'{arr[0],arr[-1]}'
-        if map_str in exist_map:
-            return exist_map[map_str]
-        for idx in range(len(arr)):
-            left = self.calculatePath(arr[:idx], exist_map)
-            right = self.calculatePath(arr[idx+1:], exist_map)
+        for i in range(1, n+1):
+            left = self.calculatePath(i-1, exist_map)
+            right = self.calculatePath(n-i, exist_map)
             res += left * right
-        exist_map[map_str] = res
+        exist_map[n] = res
         return res
 
 if __name__ == '__main__':
